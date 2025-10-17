c91adf0f74c6d4978bda43cb2a0b42913c831b92
KajalKadole <kadolekajal4@gmail.com>
2025-10-17 21:12:01 +0530

    Update main.py

--- main.py
+++ main.py
@@ -40,4 +40,4 @@ with app.app_context():
 
 if __name__ == '__main__':
     print("🚀 Team Calendar: http://127.0.0.1:5000")
-    app.run(debug=True, host='127.0.0.1', port=5000)
+    app.run(debug=True, host='0.0.0.0', port=5000)
